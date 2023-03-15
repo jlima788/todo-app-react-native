fa4b63695289b817f1461c4ebbf89b74f5015eff
Everaldo Lima <jlima788@yahoo.com.br>
2023-03-15 17:21:26 -0300

    Componente Header, Page Main e seus estilos.

---
+++ src/components/header/styles.ts
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  textHeader: {
+    fontFamily: 'Monteserrat-Bold',
+    fontSize: 22,
+  },
+  container: {
+    backgroundColor: '#f26',
+    marginBottom: 20,
+    height: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
